addd075ca951b09f7feba664f8a03738d9461bfe
VictorDMoura <mouramvictor@gmail.com>
2023-12-07 17:59:10 -0300

    feat: create POST route

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import http from "node:http";
+import { routes } from "./middlewares/routes.js";
+import { json } from "./middlewares/json.js";
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
-  if (method == "GET" && url == "/tasks") {
-    res.writeHead(200).end(JSON.stringify(tasks));
+  await json(req, res);
+  const route = routes.find((route) => {
+    return route.method == method;
+  });
+
+  if (route) {
+    return route.handler(req, res);
   }
+
   return res.writeHead(404).end();
 });
 
