780c2a3c713c88a27a3eb81d6e158ccbc35654ec
VictorDMoura <mouramvictor@gmail.com>
2023-12-07 17:04:48 -0300

    creating database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  if (method == "GET" && url == "/tasks") {
+    res.writeHead(200).end(JSON.stringify(tasks));
+  }
   return res.writeHead(404).end();
 });
 
