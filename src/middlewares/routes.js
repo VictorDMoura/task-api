0ab9168aeb8f9177a1a57f1ad71186cd606e0d82
VictorDMoura <mouramvictor@gmail.com>
2023-12-07 23:55:49 -0300

    create get method

--- src/middlewares/routes.js
+++ src/middlewares/routes.js
@@ -1,11 +1,12 @@
 import { randomUUID } from "node:crypto";
 import { Database } from "./database.js";
+import { buildRoutePath } from "../utils/build-route-path.js";
 const database = new Database();
 
 export const routes = [
   {
     method: "POST",
-    path: "/tasks",
+    path: buildRoutePath("/tasks"),
     handler: (req, res) => {
       const { title, description } = req.body;
 
@@ -19,7 +20,42 @@ export const routes = [
       };
 
       database.insert("tasks", task);
-      return res.writeHead(201).end(JSON.stringify(task));
+      return res.writeHead(201).end();
     },
   },
+  {
+    method: "GET",
+    path: buildRoutePath("/tasks"),
+    handler: (req, res) => {
+      const { query } = req;
+      const tasks = database.select("tasks");
+
+      let filterTasks = tasks;
+      if (query.title && query.description) {
+        filterTasks = tasks.filter((task) => {
+          return (
+            task.title.includes(query.title) ||
+            task.description.includes(query.description)
+          );
+        });
+      }
+
+      if (query.title && !query.description) {
+        filterTasks = tasks.filter((task) => {
+          return task.title.includes(query.title);
+        });
+      }
+
+      if (query.description && !query.title) {
+        filterTasks = tasks.filter((task) => {
+          return task.description.includes(query.description);
+        });
+      }
+
+      return res
+        .setHeader("Content-Type", "application/json")
+        .end(JSON.stringify(filterTasks));
+    },
+  },
+  {},
 ];
